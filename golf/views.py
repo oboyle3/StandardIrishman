77e8dcbad9e2080551bfc7f5957b30f51f180c52
oboyle3 <oboyle3patrick@gmail.com>
2025-09-25 23:21:54 -0400

    hit milstone 2 added ability to login and logout

--- golf/views.py
+++ golf/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
+
+
+
+@login_required
+def dashboard(request):
+    return render(request, "dashboard.html")
